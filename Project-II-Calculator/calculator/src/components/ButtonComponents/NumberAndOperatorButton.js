f09d5202ea8cbf9b599d3c25c42ba841862e43d8
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-05-28 14:51:27 +0200

    Added more comments in App.js and NumberAndOperatorButton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberAndOperatorButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberAndOperatorButton.js
@@ -13,9 +13,10 @@ ${ButtonIsOperator(props.children) ? null : "operator"}`}
         //Inline If-Else with Conditional Operator
         // method for conditionally rendering elements inline is to
         // use the JavaScript conditional operator condition ? true : false
-        // if props.chilldren(5,6,7, = ) is(?) true -> make nothing(null) , else(:) it is false -> give it the class operator
+        // if props.chilldren(5,6,7, = ) is(?) true -> make nothing(null) , else(:) if our ButtonIsOperator function is false -> give it the class operator
         onClick={() => props.handleClick(props.children)}
 // The onclick attribute fires on a mouse click on the element.
+// we must add the props.children in that case to the handleClick because we would get only objects displayed in our input field if not
 >
         {props.children}</div>
         //props.children is the textContent of Button which get's the the value inside app.js assigned
