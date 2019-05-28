dbc495ab37e5b31de0d896713dcc8e09bb4b58c1
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-05-28 16:20:36 +0200

    Changed value of key attribute again

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 export const ClearButton = props => (
-        <div className="clear-button" onClick={props.handleClear} key={props.children}>
+        <div className="clear-button" onClick={props.handleClear} key={props}>
                 {props.children}
                 {/* same funcionality like in numberButton.js */}
         </div>
